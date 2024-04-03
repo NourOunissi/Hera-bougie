0dbd51ebd1873ed1d060cc91a8c7e1eb9116bc6d
Nour Ounissi <ounissi.nour@outlook.fr>
2024-04-03 14:39:42 +0200

    supprim

--- Laravel-Hera-bougie-1-main/resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.base') {{-- pour l'heritage de la fille --}}
-
-@section('title', 'Accueil') {{-- pour le titre --}}
-
-@section('content')
-
-    <img src="/assets/img/fond.png" width="950" height="950" alt="">
-
-@endsection
--- Laravel-Hera-bougie-1-main/resources/views/script.blade.php
+++
@@ -1,3 +0,0 @@
-<script src="{{ asset('assets/lib/bootstrap/js/bootstrap.js') }}"></script>
-<script src="{{ asset('assets/lib/jquery/jquery.js') }}"></script>
-<script src="{{ asset('assets/main/user/user.js') }}"></script>
